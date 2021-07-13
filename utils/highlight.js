f5d1e236f3f36d7a00f29fec8f00bfa1d785beac
z9956 <zhz995617@gmail.com>
2021-07-13 11:47:27 +0800

    feat: highlight

---
+++ utils/highlight.js
@@ -0,0 +1,23 @@
+//https://github.com/jsfront/src/blob/master/js.md
+
+/**
+ * 文本高亮
+ * @param { string } text 文本
+ * @param { string } words 单词
+ * @param { string } tag 表情
+ * */
+function highlight(text, words, tag = 'span') {
+	let i,
+		len = words.length,
+		re;
+	for (i = 0; i < len; i++) {
+		re = new RegExp(words[i], 'g');
+		if (re.test(text)) {
+			text = text.replace(
+				re,
+				'<' + tag + ' class="highlight">$&</' + tag + '>',
+			);
+		}
+	}
+	return text;
+}
