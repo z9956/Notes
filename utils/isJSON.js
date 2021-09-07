4d028d96bf9c5ff4bb25cf7894ec6ae6ce3368af
z9956 <zhz995617@gmail.com>
2021-09-07 11:03:19 +0800

    feat: add isJSON

---
+++ utils/isJSON.js
@@ -0,0 +1,16 @@
+/**
+ * 判断是否为JSON
+ * @param { string } str 需要判断的数据
+ * */
+export const isJSON = (str) => {
+	if (typeof str == 'string') {
+		try {
+			let obj = JSON.parse(str);
+			return !!(typeof obj == 'object' && obj);
+		} catch (e) {
+			console.log('error：' + str + '!!!' + e);
+			return false;
+		}
+	}
+	console.log('It is not a string!');
+};
