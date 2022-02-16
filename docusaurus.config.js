a39c64e6fc9822007ed7d560026a69e31f6902f2
z9956 <zhz995617@gmail.com>
2022-02-16 22:43:39 +0800

    chore: chore

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -6,101 +6,99 @@ const darkCodeTheme = require('prism-react-renderer/themes/dracula');
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
+	title: 'Notes',
+	tagline: '',
+	url: 'https://notes-z9956.vercel.app/',
+	baseUrl: '/',
+	// deploymentBranch: "gh-pages",
+	onBrokenLinks: 'throw',
+	onBrokenMarkdownLinks: 'warn',
+	favicon: 'img/favicon.ico',
+	organizationName: 'z9956', // Usually your GitHub org/user name.
+	projectName: 'Notes', // Usually your repo name.
 
-  title: 'Notes',
-  tagline: '',
-  url: 'https://notes-z9956.vercel.app/',
-  baseUrl: '/',
-  // deploymentBranch: "gh-pages",
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.ico',
-  organizationName: 'z9956', // Usually your GitHub org/user name.
-  projectName: 'Notes', // Usually your repo name.
+	presets: [
+		[
+			'classic',
+			/** @type {import('@docusaurus/preset-classic').Options} */
+			({
+				docs: {
+					sidebarPath: require.resolve('./sidebars.js'),
+					// Please change this to your repo.
+					editUrl:
+						'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
+				},
+				theme: {
+					customCss: require.resolve('./src/css/custom.css'),
+				},
+			}),
+		],
+	],
 
-  presets: [
-    [
-      'classic',
-      /** @type {import('@docusaurus/preset-classic').Options} */
-      ({
-        docs: {
-          // routeBasePath: '/',
-          // path: './docs',
-          sidebarPath: require.resolve('./sidebars.js'),
-          // Please change this to your repo.
-          editUrl: 'https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/',
-        },
-        theme: {
-          customCss: require.resolve('./src/css/custom.css'),
-        },
-      }),
-    ],
-  ],
-
-  themeConfig:
-    /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
-    ({
-      navbar: {
-        items: [
-          {
-            type: 'doc',
-            docId: 'Books',
-            position: 'left',
-            label: 'Notes',
-          },
-          {
-            href: 'https://github.com/facebook/docusaurus',
-            label: 'GitHub',
-            position: 'right',
-          },
-        ],
-      },
-      footer: {
-        style: 'dark',
-        links: [
-          {
-            title: 'Docs',
-            items: [
-              {
-                label: 'Notes',
-                to: '/docs/Books',
-              },
-            ],
-          },
-          {
-            title: 'Community',
-            items: [
-              {
-                label: 'Stack Overflow',
-                href: 'https://stackoverflow.com/questions/tagged/docusaurus',
-              },
-              {
-                label: 'Discord',
-                href: 'https://discordapp.com/invite/docusaurus',
-              },
-              {
-                label: 'Twitter',
-                href: 'https://twitter.com/docusaurus',
-              },
-            ],
-          },
-          {
-            title: 'More',
-            items: [
-              {
-                label: 'GitHub',
-                href: 'https://github.com/facebook/docusaurus',
-              },
-            ],
-          },
-        ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
-      },
-      prism: {
-        theme: lightCodeTheme,
-        darkTheme: darkCodeTheme,
-      },
-    }),
+	themeConfig:
+		/** @type {import('@docusaurus/preset-classic').ThemeConfig} */
+		({
+			navbar: {
+				items: [
+					{
+						type: 'doc',
+						docId: 'Books',
+						position: 'left',
+						label: 'Notes',
+					},
+					{
+						href: 'https://github.com/z9956/Notes',
+						label: 'GitHub',
+						position: 'right',
+					},
+				],
+			},
+			footer: {
+				style: 'dark',
+				links: [
+					{
+						title: 'Docs',
+						items: [
+							{
+								label: 'Notes',
+								to: '/docs/Books',
+							},
+						],
+					},
+					{
+						title: 'Community',
+						items: [
+							{
+								label: 'Stack Overflow',
+								href: 'https://stackoverflow.com/questions/tagged/docusaurus',
+							},
+							{
+								label: 'Discord',
+								href: 'https://discordapp.com/invite/docusaurus',
+							},
+							{
+								label: 'Twitter',
+								href: 'https://twitter.com/docusaurus',
+							},
+						],
+					},
+					{
+						title: 'More',
+						items: [
+							{
+								label: 'GitHub',
+								href: 'https://github.com/facebook/docusaurus',
+							},
+						],
+					},
+				],
+				copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+			},
+			prism: {
+				theme: lightCodeTheme,
+				darkTheme: darkCodeTheme,
+			},
+		}),
 };
 
 module.exports = config;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,23 +1,24 @@
 import React, { useEffect } from 'react';
 import Layout from '@theme/Layout';
 import useDocusaurusContext from '@docusaurus/useDocusaurusContext';
-import {useHistory} from '@docusaurus/router';
+import { useHistory } from '@docusaurus/router';
 
+import styles from './index.module.css';
 
 export default function Home() {
-  const {siteConfig} = useDocusaurusContext();
+	const { siteConfig } = useDocusaurusContext();
 
-  const History = useHistory();
-  useEffect(() => {
-    History.push('/docs/');
-  }, []);
+	const History = useHistory();
+	useEffect(() => {
+		History.push('/docs/');
+	}, []);
 
-
-  return (
-    <Layout
-      title={`Hello from ${siteConfig.title}`}
-      description="Description will go into a meta tag in <head />">
-      loading...
-    </Layout>
-  );
+	return (
+		<Layout
+			title={`Hello from ${siteConfig.title}`}
+			description="Description will go into a meta tag in <head />"
+		>
+			<h5 className={styles.loading}>loading...</h5>
+		</Layout>
+	);
 }
