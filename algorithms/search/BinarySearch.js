66d7cd2099aa38c6dd371a2c632cde41b98b10d1
z9956 <zhz995617@gmail.com>
2022-11-01 09:44:26 +0800

    style: style

--- algorithms/search/BinarySearch.js
+++ algorithms/search/BinarySearch.js
@@ -1,3 +1,7 @@
+/*
+ * 二分查找
+ * https://zh.wikipedia.org/zh-cn/%E4%BA%8C%E5%88%86%E6%90%9C%E5%B0%8B%E6%BC%94%E7%AE%97%E6%B3%95
+ * */
 function binarySearch(arr, val) {
 	let start = 0;
 	let end = arr.length - 1;
