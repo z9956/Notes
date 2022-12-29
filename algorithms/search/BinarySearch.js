e107bdcd7c19f68bd86282ac6948518456cfd42b
z9956 <zhz995617@gmail.com>
2022-12-29 16:36:24 +0800

    style: style

--- algorithms/search/BinarySearch.js
+++ algorithms/search/BinarySearch.js
@@ -2,23 +2,21 @@
  * 二分查找
  * https://zh.wikipedia.org/zh-cn/%E4%BA%8C%E5%88%86%E6%90%9C%E5%B0%8B%E6%BC%94%E7%AE%97%E6%B3%95
  * */
-function binarySearch(arr, val) {
+function binarySearch(list, value) {
 	let start = 0;
-	let end = arr.length - 1;
+	let end = list.length - 1;
 
 	while (start <= end) {
-		let mid = Math.floor((start + end) / 2);
+		const mid = Math.floor((start + end) / 2);
+		if (list[mid] === value) return mid;
 
-		if (arr[mid] === val) {
-			return mid;
-		}
-
-		if (val < arr[mid]) {
-			end = mid - 1;
-		} else {
+		if (list[mid] < value) {
 			start = mid + 1;
+		} else {
+			end = mid - 1;
 		}
 	}
+
 	return -1;
 }
 
