7ae71a0503ab80c6392ede5e4cfd91cd73ac7ecc
z9956 <zhz995617@gmail.com>
2023-03-30 17:57:04 +0800

    chore: jest

---
+++ test/algorithms.js
@@ -0,0 +1,13 @@
+function bubbleSort(list) {}
+function insertionSort(list) {}
+function mergeSort(list) {}
+function quickSort(list) {}
+function selectionSort(list) {}
+
+module.exports = {
+	bubbleSort,
+	insertionSort,
+	mergeSort,
+	quickSort,
+	selectionSort,
+};
