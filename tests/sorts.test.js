a7a10752abd48270b709cb9f16aa89dd8a6bfc1e
9956 <zhz995617@gmail.com>
2024-04-01 11:49:36 +0800

    chore: Use vitest

--- tests/sorts.test.js
+++ tests/sorts.test.js
@@ -1,3 +1,5 @@
+import { expect, test } from 'vitest';
+
 import {
 	binarySearch,
 	bubbleSort,
@@ -7,107 +9,95 @@ import {
 	selectionSort,
 } from './sorts';
 
-describe('binarySearch', () => {
-	it('should sort arrays correctly', () => {
-		expect(binarySearch([1, 2, 3, 4, 5], 2)).toEqual(1);
-		expect(binarySearch([], 1)).toEqual(-1);
-		expect(binarySearch([1, 2, 3], 3)).toEqual(2);
-		expect(binarySearch([1, 2, 5, 6, 7, 8, 12, 14], 5)).toEqual(2);
-		expect(binarySearch([4, 5, 6, 7, 8, 9], 7)).toEqual(3);
-	});
+test('binarySearch', () => {
+	expect(binarySearch([1, 2, 3, 4, 5], 2)).toBe(1);
+	expect(binarySearch([], 1)).toBe(-1);
+	expect(binarySearch([1, 2, 3], 3)).toBe(2);
+	expect(binarySearch([1, 2, 5, 6, 7, 8, 12, 14], 5)).toBe(2);
+	expect(binarySearch([4, 5, 6, 7, 8, 9], 7)).toBe(3);
 });
 
-describe('bubbleSort', () => {
-	it('should sort arrays correctly', () => {
-		expect(bubbleSort([5, 4, 1, 2, 3])).toEqual([1, 2, 3, 4, 5]);
-		expect(bubbleSort([])).toEqual([]);
-		expect(bubbleSort([1, 2, 3])).toEqual([1, 2, 3]);
-		expect(bubbleSort([5, 6, 7, 8, 1, 2, 12, 14])).toEqual([
-			1, 2, 5, 6, 7, 8, 12, 14,
-		]);
-		expect(bubbleSort([5, 6, 7, 8, 9, 4])).toEqual([4, 5, 6, 7, 8, 9]);
-		expect(bubbleSort([20, 30, 40])).toEqual([20, 30, 40]);
-		expect(bubbleSort([2, 1, 3])).toEqual([1, 2, 3]);
-		expect(bubbleSort([10, 15, 16, 100])).toEqual([10, 15, 16, 100]);
-		expect(bubbleSort([10, 9, 11])).toEqual([9, 10, 11]);
-		expect(bubbleSort([10, 9, 12])).toEqual([9, 10, 12]);
-		expect(bubbleSort([3, 2, 1])).toEqual([1, 2, 3]);
-		expect(bubbleSort([10, 9, 8])).toEqual([8, 9, 10]);
-	});
+test('bubbleSort', () => {
+	expect(bubbleSort([5, 4, 1, 2, 3])).toStrictEqual([1, 2, 3, 4, 5]);
+	expect(bubbleSort([])).toStrictEqual([]);
+	expect(bubbleSort([1, 2, 3])).toStrictEqual([1, 2, 3]);
+	expect(bubbleSort([5, 6, 7, 8, 1, 2, 12, 14])).toStrictEqual([
+		1, 2, 5, 6, 7, 8, 12, 14,
+	]);
+	expect(bubbleSort([5, 6, 7, 8, 9, 4])).toStrictEqual([4, 5, 6, 7, 8, 9]);
+	expect(bubbleSort([20, 30, 40])).toStrictEqual([20, 30, 40]);
+	expect(bubbleSort([2, 1, 3])).toStrictEqual([1, 2, 3]);
+	expect(bubbleSort([10, 15, 16, 100])).toStrictEqual([10, 15, 16, 100]);
+	expect(bubbleSort([10, 9, 11])).toStrictEqual([9, 10, 11]);
+	expect(bubbleSort([10, 9, 12])).toStrictEqual([9, 10, 12]);
+	expect(bubbleSort([3, 2, 1])).toStrictEqual([1, 2, 3]);
+	expect(bubbleSort([10, 9, 8])).toStrictEqual([8, 9, 10]);
 });
 
-describe('insertionSort', () => {
-	it('should sort arrays correctly', () => {
-		expect(insertionSort([5, 4, 1, 2, 3])).toEqual([1, 2, 3, 4, 5]);
-		expect(insertionSort([])).toEqual([]);
-		expect(insertionSort([1, 2, 3])).toEqual([1, 2, 3]);
-		expect(insertionSort([5, 6, 7, 8, 1, 2, 12, 14])).toEqual([
-			1, 2, 5, 6, 7, 8, 12, 14,
-		]);
-		expect(insertionSort([5, 6, 7, 8, 9, 4])).toEqual([4, 5, 6, 7, 8, 9]);
-		expect(insertionSort([20, 30, 40])).toEqual([20, 30, 40]);
-		expect(insertionSort([2, 1, 3])).toEqual([1, 2, 3]);
-		expect(insertionSort([10, 15, 16, 100])).toEqual([10, 15, 16, 100]);
-		expect(insertionSort([10, 9, 11])).toEqual([9, 10, 11]);
-		expect(insertionSort([10, 9, 12])).toEqual([9, 10, 12]);
-		expect(insertionSort([3, 2, 1])).toEqual([1, 2, 3]);
-		expect(insertionSort([10, 9, 8])).toEqual([8, 9, 10]);
-	});
+test('insertionSort', () => {
+	expect(insertionSort([5, 4, 1, 2, 3])).toStrictEqual([1, 2, 3, 4, 5]);
+	expect(insertionSort([])).toStrictEqual([]);
+	expect(insertionSort([1, 2, 3])).toStrictEqual([1, 2, 3]);
+	expect(insertionSort([5, 6, 7, 8, 1, 2, 12, 14])).toStrictEqual([
+		1, 2, 5, 6, 7, 8, 12, 14,
+	]);
+	expect(insertionSort([5, 6, 7, 8, 9, 4])).toStrictEqual([4, 5, 6, 7, 8, 9]);
+	expect(insertionSort([20, 30, 40])).toStrictEqual([20, 30, 40]);
+	expect(insertionSort([2, 1, 3])).toStrictEqual([1, 2, 3]);
+	expect(insertionSort([10, 15, 16, 100])).toStrictEqual([10, 15, 16, 100]);
+	expect(insertionSort([10, 9, 11])).toStrictEqual([9, 10, 11]);
+	expect(insertionSort([10, 9, 12])).toStrictEqual([9, 10, 12]);
+	expect(insertionSort([3, 2, 1])).toStrictEqual([1, 2, 3]);
+	expect(insertionSort([10, 9, 8])).toStrictEqual([8, 9, 10]);
 });
 
-describe('mergeSort', () => {
-	it('should sort arrays correctly', () => {
-		expect(mergeSort([5, 4, 1, 2, 3])).toEqual([1, 2, 3, 4, 5]);
-		expect(mergeSort([])).toEqual([]);
-		expect(mergeSort([1, 2, 3])).toEqual([1, 2, 3]);
-		expect(mergeSort([5, 6, 7, 8, 1, 2, 12, 14])).toEqual([
-			1, 2, 5, 6, 7, 8, 12, 14,
-		]);
-		expect(mergeSort([5, 6, 7, 8, 9, 4])).toEqual([4, 5, 6, 7, 8, 9]);
-		expect(mergeSort([20, 30, 40])).toEqual([20, 30, 40]);
-		expect(mergeSort([2, 1, 3])).toEqual([1, 2, 3]);
-		expect(mergeSort([10, 15, 16, 100])).toEqual([10, 15, 16, 100]);
-		expect(mergeSort([10, 9, 11])).toEqual([9, 10, 11]);
-		expect(mergeSort([10, 9, 12])).toEqual([9, 10, 12]);
-		expect(mergeSort([3, 2, 1])).toEqual([1, 2, 3]);
-		expect(mergeSort([10, 9, 8])).toEqual([8, 9, 10]);
-	});
+test('mergeSort', () => {
+	expect(mergeSort([5, 4, 1, 2, 3])).toStrictEqual([1, 2, 3, 4, 5]);
+	expect(mergeSort([])).toStrictEqual([]);
+	expect(mergeSort([1, 2, 3])).toStrictEqual([1, 2, 3]);
+	expect(mergeSort([5, 6, 7, 8, 1, 2, 12, 14])).toStrictEqual([
+		1, 2, 5, 6, 7, 8, 12, 14,
+	]);
+	expect(mergeSort([5, 6, 7, 8, 9, 4])).toStrictEqual([4, 5, 6, 7, 8, 9]);
+	expect(mergeSort([20, 30, 40])).toStrictEqual([20, 30, 40]);
+	expect(mergeSort([2, 1, 3])).toStrictEqual([1, 2, 3]);
+	expect(mergeSort([10, 15, 16, 100])).toStrictEqual([10, 15, 16, 100]);
+	expect(mergeSort([10, 9, 11])).toStrictEqual([9, 10, 11]);
+	expect(mergeSort([10, 9, 12])).toStrictEqual([9, 10, 12]);
+	expect(mergeSort([3, 2, 1])).toStrictEqual([1, 2, 3]);
+	expect(mergeSort([10, 9, 8])).toStrictEqual([8, 9, 10]);
 });
 
-describe('quickSort', () => {
-	it('should sort arrays correctly', () => {
-		expect(quickSort([5, 4, 1, 2, 3])).toEqual([1, 2, 3, 4, 5]);
-		expect(quickSort([])).toEqual([]);
-		expect(quickSort([1, 2, 3])).toEqual([1, 2, 3]);
-		expect(quickSort([5, 6, 7, 8, 1, 2, 12, 14])).toEqual([
-			1, 2, 5, 6, 7, 8, 12, 14,
-		]);
-		expect(quickSort([5, 6, 7, 8, 9, 4])).toEqual([4, 5, 6, 7, 8, 9]);
-		expect(quickSort([20, 30, 40])).toEqual([20, 30, 40]);
-		expect(quickSort([2, 1, 3])).toEqual([1, 2, 3]);
-		expect(quickSort([10, 15, 16, 100])).toEqual([10, 15, 16, 100]);
-		expect(quickSort([10, 9, 11])).toEqual([9, 10, 11]);
-		expect(quickSort([10, 9, 12])).toEqual([9, 10, 12]);
-		expect(quickSort([3, 2, 1])).toEqual([1, 2, 3]);
-		expect(quickSort([10, 9, 8])).toEqual([8, 9, 10]);
-	});
+test('quickSort', () => {
+	expect(quickSort([5, 4, 1, 2, 3])).toStrictEqual([1, 2, 3, 4, 5]);
+	expect(quickSort([])).toStrictEqual([]);
+	expect(quickSort([1, 2, 3])).toStrictEqual([1, 2, 3]);
+	expect(quickSort([5, 6, 7, 8, 1, 2, 12, 14])).toStrictEqual([
+		1, 2, 5, 6, 7, 8, 12, 14,
+	]);
+	expect(quickSort([5, 6, 7, 8, 9, 4])).toStrictEqual([4, 5, 6, 7, 8, 9]);
+	expect(quickSort([20, 30, 40])).toStrictEqual([20, 30, 40]);
+	expect(quickSort([2, 1, 3])).toStrictEqual([1, 2, 3]);
+	expect(quickSort([10, 15, 16, 100])).toStrictEqual([10, 15, 16, 100]);
+	expect(quickSort([10, 9, 11])).toStrictEqual([9, 10, 11]);
+	expect(quickSort([10, 9, 12])).toStrictEqual([9, 10, 12]);
+	expect(quickSort([3, 2, 1])).toStrictEqual([1, 2, 3]);
+	expect(quickSort([10, 9, 8])).toStrictEqual([8, 9, 10]);
 });
 
-describe('selectionSort', () => {
-	it('should sort arrays correctly', () => {
-		expect(selectionSort([5, 4, 1, 2, 3])).toEqual([1, 2, 3, 4, 5]);
-		expect(selectionSort([])).toEqual([]);
-		expect(selectionSort([1, 2, 3])).toEqual([1, 2, 3]);
-		expect(selectionSort([5, 6, 7, 8, 1, 2, 12, 14])).toEqual([
-			1, 2, 5, 6, 7, 8, 12, 14,
-		]);
-		expect(selectionSort([5, 6, 7, 8, 9, 4])).toEqual([4, 5, 6, 7, 8, 9]);
-		expect(selectionSort([20, 30, 40])).toEqual([20, 30, 40]);
-		expect(selectionSort([2, 1, 3])).toEqual([1, 2, 3]);
-		expect(selectionSort([10, 15, 16, 100])).toEqual([10, 15, 16, 100]);
-		expect(selectionSort([10, 9, 11])).toEqual([9, 10, 11]);
-		expect(selectionSort([10, 9, 12])).toEqual([9, 10, 12]);
-		expect(selectionSort([3, 2, 1])).toEqual([1, 2, 3]);
-		expect(selectionSort([10, 9, 8])).toEqual([8, 9, 10]);
-	});
+test('selectionSort', () => {
+	expect(selectionSort([5, 4, 1, 2, 3])).toStrictEqual([1, 2, 3, 4, 5]);
+	expect(selectionSort([])).toStrictEqual([]);
+	expect(selectionSort([1, 2, 3])).toStrictEqual([1, 2, 3]);
+	expect(selectionSort([5, 6, 7, 8, 1, 2, 12, 14])).toStrictEqual([
+		1, 2, 5, 6, 7, 8, 12, 14,
+	]);
+	expect(selectionSort([5, 6, 7, 8, 9, 4])).toStrictEqual([4, 5, 6, 7, 8, 9]);
+	expect(selectionSort([20, 30, 40])).toStrictEqual([20, 30, 40]);
+	expect(selectionSort([2, 1, 3])).toStrictEqual([1, 2, 3]);
+	expect(selectionSort([10, 15, 16, 100])).toStrictEqual([10, 15, 16, 100]);
+	expect(selectionSort([10, 9, 11])).toStrictEqual([9, 10, 11]);
+	expect(selectionSort([10, 9, 12])).toStrictEqual([9, 10, 12]);
+	expect(selectionSort([3, 2, 1])).toStrictEqual([1, 2, 3]);
+	expect(selectionSort([10, 9, 8])).toStrictEqual([8, 9, 10]);
 });
