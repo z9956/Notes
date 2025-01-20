802f7a90e10955e7b48ca0d92304eefb5ee50c21
9956 <zhz995617@gmail.com>
2025-01-20 10:31:23 +0800

    fix: SharedGlobalState

--- src/components/sharedGlobalState/curly.jsx
+++ src/components/sharedGlobalState/curly.jsx
@@ -3,16 +3,12 @@ import global from './global.js';
 import CurlyJr from './curly.jr';
 
 export default function Curly() {
-	const decrement = () => {
-		global.counter.count = global.counter.count - 1;
-	};
-
 	return (
 		<div style={{ marginBottom: 20 }}>
 			<div style={{ float: 'left' }}>
 				<div>Curly: {global.counter.count}</div>
 				<div>
-					<button onClick={decrement}>Decrement</button>
+					<button onClick={global.counter.decrement}>Decrement</button>
 				</div>
 			</div>
 			<CurlyJr />
